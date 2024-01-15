45d82f729084e0584150e5a4ca39f9e1806c5f76
jessffff <jessicabjb@gmail.com>
2024-01-15 17:25:03 +0100

    méthode post

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 
 const app = express();
-
+app.use(express.json()); // add this line
 const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
-
+app.post("/api/movies", movieControllers.postMovie);
 // Ajout des routes pour les utilisateurs
 const userControllers = require('./controllers/userControllers');
 
 app.get('/api/users', userControllers.getUsers);
 app.get('/api/users/:id', userControllers.getUserById);
+app.post("/api/users", userControllers.postUsers);
+
+
 
 module.exports = app;
